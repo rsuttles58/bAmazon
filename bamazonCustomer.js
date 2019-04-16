4c289cd35bff8b6dd3ad27e087557c30ce12ed34
rsuttles58 <robert.suttles58@gmail.com>
2019-04-15 19:08:55 -0500

    Finalized Pt1 and Pt2. Touched up messages to customer.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,85 +30,65 @@ function start() {
             if (answer.purchase === "Yes") {
                 purchase();
             } else {
-                console.log("Come back soon!");
+                console.log("Thank you for visiting.  Please come and see our new products soon.");
                 connection.end();
             }
         })
 }
 
 function purchase() {
-    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
+    connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
-        var prods = [];
-        for (var i = 0; i < res.length; i++) {
-            console.log("ID: " + res[i].item_id + " | " + res[i].product_name + " | " + "Price: " + res[i].price)
-            prods.push([res[i].item_id, res[i].product_name, res[i].stock_quantity,res[i].price]);
-        }
 
         inquirer
-            .prompt([
-                {
-                    name: "id",
-                    type: "input",
-                    message: "Enter the ID of the item you would like to purchase ",
+          .prompt([
+              {
+                name: "choice",
+                type: "rawlist",
+                choices: function() {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name);
+                    }
+                    return choiceArray;
                 },
+                message: "What item would you like to purchase?"
+            },
                 {
                     name: "quantity",
                     type: "input",
-                    message: "How many would you like to purchase",
+                    message: "How many units would you like to purchase?"
                 }
             ])
-            .then(function (answer) {
-                var itemid = answer.id;
-                var quantWant = answer.quantity;
-                var chosenID;
-                console.log(prods);
-                console.log(itemid);
-                console.log(quantWant);
-
-                for (var i = 0; i < prods.length; i++) {
-                    if (prods[i][0] === itemid) {
-                        chosenID = prods[i][0];
-                        console.log(chosenID);
-                        if (quantWant <= prods[i][1]) {
-                            console.log("We have enough in stock!");
-                            console.log("Your total is " + (quantWant * prods[i][2]) + " .");
-                            // purchaseDBUpdate(itemid, quantWant);
-                        } else {
-                            console.log("Unfortunately, we do not have enough in stock.");
-                        }
+            .then(function(answer) {
+                var chosenProduct;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenProduct = results[i];
                     }
                 }
-
-                // NOTES_---------------------------------------------------------------------------------
-                //         //then run an update query to update that item's inventory in the database.  Subtracting the quantity
-                //         //to be purchased from the existing inventory.  IF the quantity requested is greater than the inventory,
-                //         //then send an error to the user that we do not have enough in stock.  
-                //         //Personal bonus:  Offer to sell the user the number that we have in stock.  If they say yes, then set quantity = inventory 
-                //         //and then subtract that value to zero out the inventory and thank the customer for shopping.
-                //         //show the customer the total cost of the items and then re-run the prompt
-                //         //consider building in logic that if the customer wants to buy anything else that it says "Would you like to buy anything else?" for a custom feel.
-                // });
-
-            })
-    })
-}
-
-function purchaseDBUpdate(id, quant){
-    console.log("We made it to db update.");
-    // connection.query(
-    //     "UPDATE products SET ? WHERE",
-    //     [
-    //         {
-    //             stock_quantity: stock_quantity - quant
-    //         },
-    //         {
-    //             item_id: id
-    //         }
-    //     ],
-    //     function (error) {
-    //         if (error) throw err;
-    //         start();
-    //     }
-    // );
+                if (chosenProduct.stock_quantity > parseInt(answer.quantity)) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: chosenProduct.stock_quantity - answer.quantity
+                            },
+                            {
+                                item_id: chosenProduct.item_id
+                            }
+                        ],
+                    function(error) {
+                        if (error) throw err;
+                        var checkoutTotal = chosenProduct.price * answer.quantity;
+                        console.log("Your order was placed!" + "Your total is: $" + checkoutTotal + ".");
+                        start();
+                    }
+                );
+            } else {
+                console.log("Unfortunately, we do not have enough in stock to fulfill your order.");
+                start();
+            }
+            });
+    });
 }
